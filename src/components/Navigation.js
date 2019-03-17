80c65cca9b66f519fe2e52a80fdbf866be34c051
Victor Briceño <40246864+Flaresstorm@users.noreply.github.com>
2019-03-17 19:35:18 -0400

    Add files via upload

---
+++ src/components/Navigation.js
@@ -0,0 +1,18 @@
+// this is a component
+/*import React, {Component} from 'react';
+
+class Navigation extends Component {
+  render() {
+    return(
+
+        <nav className="navbar navbar-dark bg-dark">
+        <a href="" className="text-white">
+        {this.state.title} - {this.state.ntareas}
+
+        </a>
+        </nav>
+
+    )
+  }
+}
+export default Navigation;*/
